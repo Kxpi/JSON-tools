6bccbc0d0293abf4bbbdc872a1bfc575008255de
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2022-12-09 20:51:31 +0100

    Update JsonSerializerDecorator.java

--- src/main/java/pl/put/poznan/jsontools/logic/JsonSerializerDecorator.java
+++ src/main/java/pl/put/poznan/jsontools/logic/JsonSerializerDecorator.java
@@ -2,14 +2,18 @@ package pl.put.poznan.jsontools.logic;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class JsonSerializerDecorator {
 
     JsonSerializer jsonSerializer;
-    String[] strings;
+    List<String> strings;
 
     JsonSerializerDecorator(JsonSerializer jsonSerializer, String[] strings){
         this.jsonSerializer = jsonSerializer;
-        this.strings = strings;
+        this.strings = new ArrayList<>(Arrays.asList(strings));
     }
 
     public abstract String serialize(JsonNode jsonNode);
