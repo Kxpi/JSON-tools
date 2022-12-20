ea2c0e81482eca46bf45dae96320dd13ca799470
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-12-20 21:01:16 +0100

    Add package-info to the root and logic/ packages

---
+++ src/main/java/pl/put/poznan/jsontools/logic/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package groups code that's used to provide JSON serialization and filtration.
+ * It consists of various serializers and two factories that can parse user's input and
+ * prepare a serializer suitable for processing the request.
+ */
+package pl.put.poznan.jsontools.logic;
