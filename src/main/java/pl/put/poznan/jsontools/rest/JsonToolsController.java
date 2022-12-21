a33a4cff9de96d3a07ca00eae75fa4863d7b9a0e
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-12-21 09:25:41 +0100

    Controller event logging and exception handling

--- src/main/java/pl/put/poznan/jsontools/rest/JsonToolsController.java
+++ src/main/java/pl/put/poznan/jsontools/rest/JsonToolsController.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.put.poznan.jsontools.logic.BasicSerializerFactory;
 import pl.put.poznan.jsontools.logic.CompoundSerializerFactory;
 import pl.put.poznan.jsontools.logic.JsonSerializer;
 
+import java.security.InvalidParameterException;
 import java.util.Arrays;
 
 
@@ -22,30 +25,41 @@ public class JsonToolsController {
     public String get(
             @RequestBody String inputJson,
             @RequestParam(name="format", defaultValue="minified") String format,
-            @RequestParam(name="action", defaultValue="echo") String action) throws JsonProcessingException {
+            @RequestParam(name="action", defaultValue="echo") String action) {
 
         // log the parameters
-        logger.debug(format);
-        logger.debug(action);
+        logger.info("New incoming request");
+        logger.debug("Requested format: " + format + ", action(s) to perform: " + action);
 
         // Deserialize the JSON
-        // TODO: Handle exceptions and remove `throws` from signature
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode parsedJson = mapper.readTree(inputJson);
+        String outputJson;
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode parsedJson = mapper.readTree(inputJson);
 
-        // Prepare serializer based on the request
-        BasicSerializerFactory bsFactory = new BasicSerializerFactory();
-        CompoundSerializerFactory csFactory = new CompoundSerializerFactory();
+            // Prepare serializer based on the request
+            BasicSerializerFactory bsFactory = new BasicSerializerFactory();
+            CompoundSerializerFactory csFactory = new CompoundSerializerFactory();
 
-        // First, create base serializer (one that converts an object to JSON)
-        // And then, extend its functionality by making more complex serializer over it
-        JsonSerializer serializer;
-        serializer = bsFactory.create(format);
-        serializer = csFactory.create(action, serializer);
+            // First, create base serializer (one that converts an object to JSON)
+            // And then, extend its functionality by making a more complex serializer over it
+            JsonSerializer serializer;
+            serializer = bsFactory.create(format);
+            serializer = csFactory.create(action, serializer);
 
-        // Perform transforms here
-        String outputJson = serializer.serialize(parsedJson);
+            // Perform transforms here
+            outputJson = serializer.serialize(parsedJson);
+        }catch(JsonProcessingException e){
+            logger.warn("Invalid JSON was sent with the request");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "The requested JSON is not well-formed.", e);
+        }catch(InvalidParameterException e){
+            logger.warn("Invalid action was requested: " + e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    e.getMessage(), e);
+        }
 
+        logger.info("Processing finished successfully");
         return outputJson;
     }
 }
