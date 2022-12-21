1ff324f37a3213c71556864ce9bd59439fb70029
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2022-12-21 14:51:27 +0100

    comments and debug messages for keep and decorator classes

--- src/main/java/pl/put/poznan/jsontools/logic/FilterKeepJsonSerializer.java
+++ src/main/java/pl/put/poznan/jsontools/logic/FilterKeepJsonSerializer.java
@@ -2,20 +2,37 @@ package pl.put.poznan.jsontools.logic;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Class that manages deleting properties from JSON file
+ */
 public class FilterKeepJsonSerializer extends JsonSerializerDecorator {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(FilterKeepJsonSerializer.class);
+    /**
+     * Creates a FilterKeepJsonSerializer object
+     * @param jsonSerializer serialzer used to return parsed json
+     * @param strings array of keept JSON parameters
+     */
     FilterKeepJsonSerializer(JsonSerializer jsonSerializer, String[] strings) {
         super(jsonSerializer, strings);
+        logger.debug("Created FilterKeepJsonSerializer object");
     }
-
+    
+    /**
+     * Method that delete omited parameters of JSON file
+     * @param jsonNode formatted JSON file
+     * @return serialized JSON file in String format
+     */
     @Override
     public String serialize(JsonNode jsonNode){
+        logger.debug("Deleting ommited properties of given JSON");
         List<String> keys = new ArrayList<>();
         Iterator<String> iterator = jsonNode.fieldNames();
         iterator.forEachRemaining(e -> keys.add(e));
