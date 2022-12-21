92921b4e44742f9f7ae608d4e88e475a0b94c2e8
Michal Szmanda <michal.szmanda@student.put.poznan.pl>
2022-12-21 13:16:59 +0100

    Comments and event logging for basic serializers

--- src/main/java/pl/put/poznan/jsontools/logic/BasicSerializerFactory.java
+++ src/main/java/pl/put/poznan/jsontools/logic/BasicSerializerFactory.java
@@ -1,11 +1,23 @@
 package pl.put.poznan.jsontools.logic;
 
 import java.security.InvalidParameterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasicSerializerFactory {
 
-    public JsonSerializer create(String outputFormat) throws InvalidParameterException{
-        switch(outputFormat){
+    private static final Logger logger = LoggerFactory.getLogger(MinifyJsonSerializer.class);
+
+    /**
+     * Creates a basic JsonSerializer for formatting JSON output
+     * 
+     * @param outputFormat accepts: "minified" or "pretty"
+     * @return basic JsonSerializer (either minifying or prettifying)
+     * @throws InvalidParameterException
+     */
+    public JsonSerializer create(String outputFormat) throws InvalidParameterException {
+        logger.debug("Creating basic JsonSerializer using a factory");
+        switch (outputFormat) {
             case "minified":
                 return new MinifyJsonSerializer();
             case "pretty":
--- src/main/java/pl/put/poznan/jsontools/logic/MinifyJsonSerializer.java
+++ src/main/java/pl/put/poznan/jsontools/logic/MinifyJsonSerializer.java
@@ -1,12 +1,27 @@
 package pl.put.poznan.jsontools.logic;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Usage: new MinifyJsonSerializer().serialize(jsonNode);
+ * Class for formatting JSON output as string
  */
 public class MinifyJsonSerializer implements JsonSerializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(MinifyJsonSerializer.class);
+
+    public MinifyJsonSerializer() {
+        logger.debug("Created MinifyJsonSerializer");
+    }
+
+    /**
+     * Converts a JSON to a String
+     * @param jsonNode JSON to convert
+     * @return minified String
+     */
     @Override
     public String serialize(JsonNode jsonNode) {
+        logger.debug("Minifying JSON");
         return jsonNode.toString();
     }
 }
