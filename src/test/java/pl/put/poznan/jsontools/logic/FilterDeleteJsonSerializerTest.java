c38c22b52ed55899e0d30641abb74058c870b1e2
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-01-25 20:55:39 +0100

    Additional mock-based tests (7 in total)

--- src/test/java/pl/put/poznan/jsontools/logic/FilterDeleteJsonSerializerTest.java
+++ src/test/java/pl/put/poznan/jsontools/logic/FilterDeleteJsonSerializerTest.java
@@ -10,10 +10,10 @@ import java.util.Iterator;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class FilterKeepJsonSerializerTest {
+public class FilterDeleteJsonSerializerTest {
 
     @Test
-    void testKeep(){
+    void testDelete(){
         JsonSerializer baseSerializer = mock(JsonSerializer.class);
         when(baseSerializer.serialize(any(JsonNode.class)))
                 .thenAnswer(invocation -> {
@@ -35,15 +35,15 @@ public class FilterKeepJsonSerializerTest {
         inputNode.put("c", "lorem");
         inputNode.put("d", "test");
 
-        String[] keys = { "a", "b" };
-        JsonSerializer serializer = new FilterKeepJsonSerializer(baseSerializer, keys);
+        String[] keys = { "c", "d" };
+        JsonSerializer serializer = new FilterDeleteJsonSerializer(baseSerializer, keys);
         String serialized = serializer.serialize(inputNode);
 
         assertEquals("a\nb\n", serialized);
     }
 
     @Test
-    void testKeepNotExists(){
+    void testDeleteNotExists(){
         JsonSerializer baseSerializer = mock(JsonSerializer.class);
         when(baseSerializer.serialize(any(JsonNode.class)))
                 .thenAnswer(invocation -> {
@@ -65,15 +65,45 @@ public class FilterKeepJsonSerializerTest {
         inputNode.put("c", "lorem");
         inputNode.put("d", "test");
 
-        String[] keys = { "a", "b", "e" };
-        JsonSerializer serializer = new FilterKeepJsonSerializer(baseSerializer, keys);
+        String[] keys = { "c", "d", "e" };
+        JsonSerializer serializer = new FilterDeleteJsonSerializer(baseSerializer, keys);
         String serialized = serializer.serialize(inputNode);
 
         assertEquals("a\nb\n", serialized);
     }
 
     @Test
-    void testKeepEmptyResult(){
+    void testDeleteEmptyResult(){
+        JsonSerializer baseSerializer = mock(JsonSerializer.class);
+        when(baseSerializer.serialize(any(JsonNode.class)))
+                .thenAnswer(invocation -> {
+                    Object[] args = invocation.getArguments();
+                    JsonNode node = (JsonNode)args[0];
+
+                    StringBuilder sb = new StringBuilder();
+                    for (Iterator<String> it = node.fieldNames(); it.hasNext(); ) {
+                        String key = it.next();
+                        sb.append(key);
+                        sb.append("\n");
+                    }
+                    return sb.toString();
+                });
+
+        ObjectNode inputNode = JsonNodeFactory.instance.objectNode();
+        inputNode.put("a", "xyz");
+        inputNode.put("b", 123);
+        inputNode.put("c", "lorem");
+        inputNode.put("d", "test");
+
+        String[] keys = { "a", "b", "c", "d" };
+        JsonSerializer serializer = new FilterDeleteJsonSerializer(baseSerializer, keys);
+        String serialized = serializer.serialize(inputNode);
+
+        assertEquals("", serialized);
+    }
+
+    @Test
+    void testDeleteEmptyRule(){
         JsonSerializer baseSerializer = mock(JsonSerializer.class);
         when(baseSerializer.serialize(any(JsonNode.class)))
                 .thenAnswer(invocation -> {
@@ -96,9 +126,9 @@ public class FilterKeepJsonSerializerTest {
         inputNode.put("d", "test");
 
         String[] keys = {};
-        JsonSerializer serializer = new FilterKeepJsonSerializer(baseSerializer, keys);
+        JsonSerializer serializer = new FilterDeleteJsonSerializer(baseSerializer, keys);
         String serialized = serializer.serialize(inputNode);
 
-        assertEquals("", serialized);
+        assertEquals("a\nb\nc\nd\n", serialized);
     }
 }
