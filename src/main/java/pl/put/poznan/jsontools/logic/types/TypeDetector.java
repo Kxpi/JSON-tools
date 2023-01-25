ed1a5fb5fd33bd137230496876950f1485e337cf
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2023-01-25 14:28:34 +0100

    added functionality

--- src/main/java/pl/put/poznan/jsontools/logic/types/TypeDetector.java
+++ src/main/java/pl/put/poznan/jsontools/logic/types/TypeDetector.java
@@ -4,7 +4,30 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class TypeDetector {
 
-    public ValueType getTypeForNode(JsonNode node){
+    static public ValueType getTypeForNode(JsonNode node){
+        if (node.isValueNode()) {
+            if (node.isTextual()) {
+                return ValueType.STRING;
+            } else if (node.isInt()) {
+                return ValueType.NUMBER;
+            } else if (node.isLong()) {
+                return ValueType.NUMBER;
+            } else if (node.isDouble()) {
+                return ValueType.NUMBER;
+            } else if (node.isBoolean()) {
+                return ValueType.BOOLEAN;
+            } else if (node.isBigDecimal()) {
+                return ValueType.NUMBER;
+            } else if (node.isBigInteger()) {
+                return ValueType.NUMBER;
+            } else if (node.isNull() || node.isMissingNode()) {
+                return ValueType.NULL;
+            }
+        } else if (node.isArray()) {
+            return ValueType.ARRAY;
+        } else if (node.isObject()) {
+            return ValueType.OBJECT;
+        }
         return ValueType.UNKNOWN;
     }
 }
